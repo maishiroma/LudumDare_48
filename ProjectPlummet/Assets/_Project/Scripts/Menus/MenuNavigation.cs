4231cb9e9befa116a50da2878eb1de5632f0eedf
Matthew Shiroma <19522250+maishiroma@users.noreply.github.com>
2021-04-24 11:07:29 -0700

    Added main menu logic

--- ProjectPlummet/Assets/_Project/Scripts/Menus/MenuNavigation.cs
+++ ProjectPlummet/Assets/_Project/Scripts/Menus/MenuNavigation.cs
@@ -4,11 +4,52 @@
     using System.Collections.Generic;
     using UnityEngine;
     using UnityEngine.SceneManagement;
+    using UnityEngine.EventSystems;
 
     public class MenuNavigation : MonoBehaviour
     {
         public int mainLevelIndex;
         public int titleScreenIndex;
+
+        public EventSystem menuEvents;
+        public GameObject mainMenuPanel;
+        public GameObject controlsPanel;
+        public GameObject creditsPanel;
+
+        private GameObject prevButton;
+
+        public void NavigateToSubButtons(string type)
+        {
+            if(mainMenuPanel != null)
+            {
+                prevButton = menuEvents.currentSelectedGameObject;
+
+                mainMenuPanel.SetActive(false);
+                switch(type)
+                {
+                    case "controls":
+                        controlsPanel.SetActive(true);
+                        break;
+                    case "credits":
+                        creditsPanel.SetActive(true);
+                        break;
+                }
+
+                menuEvents.SetSelectedGameObject(GameObject.FindGameObjectWithTag("UI_Return"));
+            }
+        }
+
+        public void NavigateBackToMain()
+        {
+            if (mainMenuPanel != null)
+            {
+                controlsPanel.SetActive(false);
+                creditsPanel.SetActive(false);
+                mainMenuPanel.SetActive(true);
+
+                menuEvents.SetSelectedGameObject(prevButton);
+            }
+        }
 
         public void LoadToMainLevel()
         {
@@ -18,6 +59,11 @@
         public void LoadToTitleScreen()
         {
             SceneManager.LoadScene(titleScreenIndex);
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
         }
 
     }

aa2a628bb616c5f896dba9ade0d85361d03ef243
Matthew Shiroma <19522250+maishiroma@users.noreply.github.com>
2021-04-24 09:57:43 -0700

    Added hud

---
+++ ProjectPlummet/Assets/_Project/Scripts/Level/HUD.cs
@@ -0,0 +1,26 @@
+﻿namespace Level
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using TMPro;
+    using Global;
+
+    public class HUD : MonoBehaviour
+    {
+        public TextMeshProUGUI scoreText;
+
+        private void Update()
+        {
+            if(GameManager.Instance != null)
+            {
+                scoreText.text = "Score: " + GameManager.Instance.Score.ToString();
+            }
+            else
+            {
+                scoreText.text = "Score: ";
+            }
+        }
+    }
+
+}

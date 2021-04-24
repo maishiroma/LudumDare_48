ed21d623139fc3f916b392cacf1de6e96250b415
Matthew Shiroma <19522250+maishiroma@users.noreply.github.com>
2021-04-23 20:31:30 -0700

    Added basic movement

---
+++ ProjectPlummet/Assets/_Project/Scripts/Global/InputActions.cs
@@ -0,0 +1,391 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/InputActions.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+namespace Global
+{
+    public class @InputActions : IInputActionCollection, IDisposable
+    {
+        public InputActionAsset asset { get; }
+        public @InputActions()
+        {
+            asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputActions"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""44651ae7-4a14-467b-b319-025685c3911c"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Button"",
+                    ""id"": ""34d9ec13-dabb-4865-a294-b090d74f9405"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""FastFall"",
+                    ""type"": ""Button"",
+                    ""id"": ""06c6153c-97b4-4d39-a402-27736a29e335"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""Arrows"",
+                    ""id"": ""e78869ec-342e-4a48-8811-cef52d6f2d47"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""52632d64-611f-4650-bc5b-2411d471c250"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""d815d170-595c-4189-8add-335101aa43a8"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""4f14894e-d5bb-43e0-b270-2d3e9d2f64f5"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""79948936-ca94-4049-8af0-d3c54a5b2568"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""313fc59c-07c1-417a-b7a9-af9c10c15544"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""de8b8273-0145-41c8-8f11-2ac52d01e8d9"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""FastFall"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        },
+        {
+            ""name"": ""Menu"",
+            ""id"": ""964b09ea-c013-4b09-9a67-25f58a0b8c34"",
+            ""actions"": [
+                {
+                    ""name"": ""Menu"",
+                    ""type"": ""Button"",
+                    ""id"": ""5fb336c1-8b20-4bcf-a2a5-ff0bbee5db4d"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Select"",
+                    ""type"": ""Button"",
+                    ""id"": ""2c2a52d6-817c-4cb2-b78a-5a63934727fa"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""Arrows"",
+                    ""id"": ""ea6967d5-de23-4f9a-b271-c5f2fd80eb1e"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Menu"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""fbffdf9d-5d5a-4279-802a-6dfec77d18e7"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Menu"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""195e6e0e-126f-4fad-931e-d9161fef7c77"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Menu"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""4f2eaaa5-379d-4277-87a4-a0d8671d497b"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Menu"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""dfd4f620-efa3-4e39-8cca-15460ab5abe6"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Menu"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""1da0e179-990f-4ba9-8b07-187fea426fcd"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Menu"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""bb3ec972-942c-447d-b6fc-a8cbaff46df6"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Select"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Keyboard"",
+            ""bindingGroup"": ""Keyboard"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+            // Player
+            m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+            m_Player_Movement = m_Player.FindAction("Movement", throwIfNotFound: true);
+            m_Player_FastFall = m_Player.FindAction("FastFall", throwIfNotFound: true);
+            // Menu
+            m_Menu = asset.FindActionMap("Menu", throwIfNotFound: true);
+            m_Menu_Menu = m_Menu.FindAction("Menu", throwIfNotFound: true);
+            m_Menu_Select = m_Menu.FindAction("Select", throwIfNotFound: true);
+        }
+
+        public void Dispose()
+        {
+            UnityEngine.Object.Destroy(asset);
+        }
+
+        public InputBinding? bindingMask
+        {
+            get => asset.bindingMask;
+            set => asset.bindingMask = value;
+        }
+
+        public ReadOnlyArray<InputDevice>? devices
+        {
+            get => asset.devices;
+            set => asset.devices = value;
+        }
+
+        public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+        public bool Contains(InputAction action)
+        {
+            return asset.Contains(action);
+        }
+
+        public IEnumerator<InputAction> GetEnumerator()
+        {
+            return asset.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Enable()
+        {
+            asset.Enable();
+        }
+
+        public void Disable()
+        {
+            asset.Disable();
+        }
+
+        // Player
+        private readonly InputActionMap m_Player;
+        private IPlayerActions m_PlayerActionsCallbackInterface;
+        private readonly InputAction m_Player_Movement;
+        private readonly InputAction m_Player_FastFall;
+        public struct PlayerActions
+        {
+            private @InputActions m_Wrapper;
+            public PlayerActions(@InputActions wrapper) { m_Wrapper = wrapper; }
+            public InputAction @Movement => m_Wrapper.m_Player_Movement;
+            public InputAction @FastFall => m_Wrapper.m_Player_FastFall;
+            public InputActionMap Get() { return m_Wrapper.m_Player; }
+            public void Enable() { Get().Enable(); }
+            public void Disable() { Get().Disable(); }
+            public bool enabled => Get().enabled;
+            public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+            public void SetCallbacks(IPlayerActions instance)
+            {
+                if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+                {
+                    @Movement.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                    @Movement.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                    @Movement.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                    @FastFall.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnFastFall;
+                    @FastFall.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnFastFall;
+                    @FastFall.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnFastFall;
+                }
+                m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+                if (instance != null)
+                {
+                    @Movement.started += instance.OnMovement;
+                    @Movement.performed += instance.OnMovement;
+                    @Movement.canceled += instance.OnMovement;
+                    @FastFall.started += instance.OnFastFall;
+                    @FastFall.performed += instance.OnFastFall;
+                    @FastFall.canceled += instance.OnFastFall;
+                }
+            }
+        }
+        public PlayerActions @Player => new PlayerActions(this);
+
+        // Menu
+        private readonly InputActionMap m_Menu;
+        private IMenuActions m_MenuActionsCallbackInterface;
+        private readonly InputAction m_Menu_Menu;
+        private readonly InputAction m_Menu_Select;
+        public struct MenuActions
+        {
+            private @InputActions m_Wrapper;
+            public MenuActions(@InputActions wrapper) { m_Wrapper = wrapper; }
+            public InputAction @Menu => m_Wrapper.m_Menu_Menu;
+            public InputAction @Select => m_Wrapper.m_Menu_Select;
+            public InputActionMap Get() { return m_Wrapper.m_Menu; }
+            public void Enable() { Get().Enable(); }
+            public void Disable() { Get().Disable(); }
+            public bool enabled => Get().enabled;
+            public static implicit operator InputActionMap(MenuActions set) { return set.Get(); }
+            public void SetCallbacks(IMenuActions instance)
+            {
+                if (m_Wrapper.m_MenuActionsCallbackInterface != null)
+                {
+                    @Menu.started -= m_Wrapper.m_MenuActionsCallbackInterface.OnMenu;
+                    @Menu.performed -= m_Wrapper.m_MenuActionsCallbackInterface.OnMenu;
+                    @Menu.canceled -= m_Wrapper.m_MenuActionsCallbackInterface.OnMenu;
+                    @Select.started -= m_Wrapper.m_MenuActionsCallbackInterface.OnSelect;
+                    @Select.performed -= m_Wrapper.m_MenuActionsCallbackInterface.OnSelect;
+                    @Select.canceled -= m_Wrapper.m_MenuActionsCallbackInterface.OnSelect;
+                }
+                m_Wrapper.m_MenuActionsCallbackInterface = instance;
+                if (instance != null)
+                {
+                    @Menu.started += instance.OnMenu;
+                    @Menu.performed += instance.OnMenu;
+                    @Menu.canceled += instance.OnMenu;
+                    @Select.started += instance.OnSelect;
+                    @Select.performed += instance.OnSelect;
+                    @Select.canceled += instance.OnSelect;
+                }
+            }
+        }
+        public MenuActions @Menu => new MenuActions(this);
+        private int m_KeyboardSchemeIndex = -1;
+        public InputControlScheme KeyboardScheme
+        {
+            get
+            {
+                if (m_KeyboardSchemeIndex == -1) m_KeyboardSchemeIndex = asset.FindControlSchemeIndex("Keyboard");
+                return asset.controlSchemes[m_KeyboardSchemeIndex];
+            }
+        }
+        public interface IPlayerActions
+        {
+            void OnMovement(InputAction.CallbackContext context);
+            void OnFastFall(InputAction.CallbackContext context);
+        }
+        public interface IMenuActions
+        {
+            void OnMenu(InputAction.CallbackContext context);
+            void OnSelect(InputAction.CallbackContext context);
+        }
+    }
+}
---
+++ ProjectPlummet/Assets/_Project/Scripts/Player/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿namespace Player
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.InputSystem;
+    using Global;
+
+    public class PlayerController : MonoBehaviour
+    {
+        public float horizontalSpeed;
+        public float maxHorizontalSpeed;
+
+        private Rigidbody2D playerRB;
+        private float movementInput;
+
+        private InputActions controls;
+
+        // Activates all of the controls for the player
+        private void Awake()
+        {
+            // We need to first set this to be a new object before we can do anything
+            controls = new InputActions();
+
+            // Then we can set up calllbacks to specific methods that we want the controls to listen to
+            controls.Player.Movement.performed += ctx => GrabMovement(ctx);
+            controls.Player.Movement.canceled += ctx => GrabMovement(ctx);
+        }
+
+        // Enables the controls when the player is active
+        private void OnEnable()
+        {
+            controls.Enable();
+        }
+
+        // Diables the controls when the player is not active
+        private void OnDisable()
+        {
+            controls.Disable();
+        }
+
+        // Sets private Components
+        private void Start()
+        {
+            movementInput = 0f;
+
+            playerRB = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            if (movementInput != 0f)
+            {
+                float newXMovement = Mathf.Clamp(playerRB.velocity.x + (movementInput * horizontalSpeed), 0f, maxHorizontalSpeed);
+
+                Vector2 newVelocity = new Vector2(newXMovement, playerRB.velocity.y);
+                playerRB.velocity = Vector2.Lerp(playerRB.velocity, newVelocity, 0.5f * Time.deltaTime);
+            }
+            else
+            {
+                Vector2 neutral = new Vector2(0f, playerRB.velocity.y);
+                playerRB.velocity = Vector2.Lerp(playerRB.velocity, neutral, 0.5f * Time.deltaTime);
+            }
+        }
+
+        private void GrabMovement(InputAction.CallbackContext ctx)
+        {
+            movementInput = ctx.ReadValue<float>();
+        }
+    }
+
+}

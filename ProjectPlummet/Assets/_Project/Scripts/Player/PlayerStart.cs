ec6635b667dc2ebd957fc52e230c0851d529378a
Matthew Shiroma <19522250+maishiroma@users.noreply.github.com>
2021-04-25 12:36:36 -0700

    Added jump to anim

--- ProjectPlummet/Assets/_Project/Scripts/Player/PlayerStart.cs
+++ ProjectPlummet/Assets/_Project/Scripts/Player/PlayerStart.cs
@@ -8,12 +8,22 @@
     public class PlayerStart : MonoBehaviour
     {
         public SpawnPlatform spawnPlatform;
+        public AudioSource sfx;
+        public AudioClip jumpSound;
 
         public void InvokeDissapear()
         {
             if(spawnPlatform != null)
             {
                 spawnPlatform.StartDissapear();
+            }
+        }
+
+        public void PlayJumpSound()
+        {
+            if(sfx != null)
+            {
+                sfx.PlayOneShot(jumpSound, 1f);
             }
         }
  
